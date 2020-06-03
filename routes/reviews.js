620ee056e2b8ecf994b30c65e8aeacd5d025fb84
madeleinemarie <maddy.vandenheuvel@gmail.com>
2020-06-03 10:36:38 -0700

    Add isLoggedIn middleware to routes for adding/updating reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const reviewCtrl = require('../controllers/reviews');
 
-router.get('/restaurants/:id/tacos/:tacoid/reviews/new', reviewCtrl.new);
-router.get('/restaurants/:id/tacos/:tacoid/reviews/:reviewid/edit', reviewCtrl.edit);
-router.put('/restaurants/:id/tacos/:tacoid/reviews/:reviewid', reviewCtrl.update);
-router.post('/restaurants/:id/tacos/:tacoid/reviews', reviewCtrl.create);
-router.delete('/restaurants/:id/tacos/:tacoid/reviews/:reviewid', reviewCtrl.delete);
+router.get('/restaurants/:id/tacos/:tacoid/reviews/new', isLoggedIn, reviewCtrl.new);
+router.get('/restaurants/:id/tacos/:tacoid/reviews/:reviewid/edit', isLoggedIn, reviewCtrl.edit);
+router.put('/restaurants/:id/tacos/:tacoid/reviews/:reviewid', isLoggedIn, reviewCtrl.update);
+router.post('/restaurants/:id/tacos/:tacoid/reviews', isLoggedIn, reviewCtrl.create);
+router.delete('/restaurants/:id/tacos/:tacoid/reviews/:reviewid', isLoggedIn, reviewCtrl.delete);
+
+function isLoggedIn(req, res, next) {
+    if ( req.isAuthenticated() ) return next();
+    res.redirect('/auth/google');
+}
 
 module.exports = router;
