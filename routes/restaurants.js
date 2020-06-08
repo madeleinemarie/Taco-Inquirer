588626968996ee5e7315c85fde569a125f22a596
madeleinemarie <maddy.vandenheuvel@gmail.com>
2020-06-07 20:12:53 -0700

    Create search route and not found route

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -4,6 +4,8 @@ const restaurantsCtrl = require('../controllers/restaurants');
 
 router.get('/', restaurantsCtrl.index);
 router.get('/new', isLoggedIn, restaurantsCtrl.new);
+router.post('/search', restaurantsCtrl.search);
+router.get('/notfound', restaurantsCtrl.notFound);
 router.get('/:id', restaurantsCtrl.show);
 router.post('/', isLoggedIn, restaurantsCtrl.create);
 
