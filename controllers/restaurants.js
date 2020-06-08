a149e7e04ceb4a6e1c9e32fbe6684c23158a72a8
madeleinemarie <maddy.vandenheuvel@gmail.com>
2020-06-07 20:07:07 -0700

    Add controller function for search results

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -5,6 +5,8 @@ module.exports = {
     index,
     show,
     new: newRestaurant,
+    search,
+    notFound,
     create
 }
 
@@ -12,6 +14,39 @@ function index(req, res){
     Restaurant.find({}, function(err, restaurants) {
         res.render('restaurants', { restaurants });
     })
+}
+
+function search(req, res){
+    console.log("req.body: " + req.body);
+    console.log("req.body.search: " + req.body.search);
+    let reqName = req.body.search;
+    console.log("reqName: " + reqName);
+    Restaurant.findOne({ name: new RegExp('^'+ reqName + '$', "i") })
+    .populate({
+        path: 'tacos',
+        populate: {
+            path: 'reviews', 
+            model: 'Review',
+            populate: {path: 'author', model: 'User'}
+        }
+    }).
+    exec(function (err, restaurant) {
+        if (err) {
+            console.log(req.body)
+            res.render('/restaurants/notfound');
+        } else {
+            if (restaurant) {
+                console.log(restaurant);
+                res.redirect(`/restaurants/${restaurant._id}`);
+            } else {
+                res.redirect('/restaurants/notfound');
+            } 
+        }
+    })
+}
+
+function notFound(req, res){
+    res.render('restaurants/notfound');
 }
 
 function show(req, res) {
@@ -27,7 +62,7 @@ function show(req, res) {
     exec(function (err, restaurant) {        
         if (err) {
             console.log("error:" + err);
-            res.render('restaurants/show');
+            res.render('restaurants/');
         } else {
             console.log("restaurant:" + restaurant);
             res.render('restaurants/show', { restaurant });
@@ -43,7 +78,7 @@ function create(req, res){
     const restaurant = new Restaurant(req.body);
     restaurant.save(function (err){
         if (err) return console.error(err);
-        res.redirect('/restaurants');
+        res.redirect(`/restaurants/${restaurant._id}`);
     });
 }
 
